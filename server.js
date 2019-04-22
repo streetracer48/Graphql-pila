6a96f998740605eeff3c1cacf0ab3da53a3ce685
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-04-22 19:19:16 +0600

    server and mongodb connect

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
-
+const mongoose = require("mongoose")
+// DB Config
+const db = require("./config/keys").mongoURI;
 // Initializes application
 const app = express();
+
+// Connect to MongoDB
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 
 const PORT = process.env.PORT || 4444;
