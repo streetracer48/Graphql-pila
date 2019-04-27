6ea0f098202e9e709cd5cae9b3eb41f1dfd33b00
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-04-27 21:51:27 +0600

    mutation and implement in index signup

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react'
-
+import {Mutation} from "react-apollo";
+import {SIGNUP_USER } from "../../queries";
 const initialState = {
     username: "",
     email: "",
@@ -27,13 +28,13 @@ const initialState = {
       handleSubmit = (event, signupUser) => {
         event.preventDefault();
         console.log(this.state)
-        // signupUser().then(async ({ data }) => {
-        //   // console.log(data);
+        signupUser().then(async ({ data }) => {
+          console.log(data);
         //   localStorage.setItem("token", data.signupUser.token);
         //   await this.props.refetch();
         //   this.clearState();
         //   this.props.history.push("/");
-        // });
+        });
       };
 
   render() {
@@ -42,48 +43,56 @@ const { username, email, password, passwordConfirmation } = this.state;
     return (
         <div className="App">
         <h2 className="App">Signup</h2>
-            <form
-                className="form"
-                onSubmit={event => this.handleSubmit(event,)}
-              >
-                <input
-                  type="text"
-                  name="username"
-                  placeholder="Username"
-                  value={username}
-                  onChange={this.handleChange}
-                />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email Address"
-                  value={email}
-                  onChange={this.handleChange}
-                />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={this.handleChange}
-                />
-                <input
-                  type="password"
-                  name="passwordConfirmation"
-                  placeholder="Confirm Password"
-                  value={passwordConfirmation}
-                  onChange={this.handleChange}
-                />
-                <button
-                  type="submit"
-                  disabled={this.validateForm()}
-                  className="button-primary"
-                >
-                  Submit
-                </button>
-                {/* {error && <Error error={error} />} */}
-              </form>
-        
+<Mutation mutation={SIGNUP_USER} variable={{username, email, password}}>
+
+{(signupUser, {data, loading, error}) => {
+     return (
+        <form
+        className="form"
+        onSubmit={event => this.handleSubmit(event, signupUser)}
+      >
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          value={email}
+          onChange={this.handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={this.handleChange}
+        />
+        <input
+          type="password"
+          name="passwordConfirmation"
+          placeholder="Confirm Password"
+          value={passwordConfirmation}
+          onChange={this.handleChange}
+        />
+        <button
+          type="submit"
+          disabled={this.validateForm()}
+          className="button-primary"
+        >
+          Submit
+        </button>
+        {/* {error && <Error error={error} />} */}
+      </form>
+     )
+}}
+
+           
+              </Mutation>
       </div>
     )
   }
