98f3150e1c3b5a27780ccd5774dfe3bba90761cc
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-04-29 21:30:14 +0600

    initalState setup

--- client/src/components/Recipe/AddRecipe.jsx
+++ client/src/components/Recipe/AddRecipe.jsx
@@ -1,5 +1,16 @@
 import React, {Component} from 'react'
 
+const initialState = {
+    name: "",
+    imageUrl: "",
+    instructions: "",
+    category: "Breakfast",
+    description: "",
+    username: ""
+  };
+  
+
+
 class AddRecipe extends Component {
 
      render() {
