eb02b55e2dfb34e6592dc2b71bdcfc8fe6538c65
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-05-01 22:24:25 +0600

    show favorites on profile sections

--- client/src/components/Profile/UserInfo.jsx
+++ client/src/components/Profile/UserInfo.jsx
@@ -16,6 +16,22 @@ const UserInfo = ({ session }) => (
     <p>Username: {session.getCurrentUser.username}</p>
     <p>Email: {session.getCurrentUser.email}</p>
     <p>Join Date: {formatDate(session.getCurrentUser.joinDate)}</p>
+
+    <ul>
+      <h3>{session.getCurrentUser.username}'s Favorites</h3>
+      {session.getCurrentUser.favorites.map(favorite => (
+        <li key={favorite._id}>
+          <Link to={`/recipes/${favorite._id}`}>
+            <p>{favorite.name}</p>
+          </Link>
+        </li>
+      ))}
+      {!session.getCurrentUser.favorites.length && (
+        <p>
+          <strong>You have no favorites currently. Go add some!</strong>
+        </p>
+      )}
+    </ul>
     </div>
 
 )
