31e0ccdc5d4e1b5e6f3d92f6ba96ac8c8b752d43
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-05-01 23:33:38 +0600

    get users clinet

--- client/src/components/Profile/UserRecipe.jsx
+++ client/src/components/Profile/UserRecipe.jsx
@@ -1,13 +1,40 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Query, Mutation } from "react-apollo";
+import {
+    GET_USER_RECIPES
+  } from "../../queries";
+  import Spinner from "../Spinner";
 
 
+const UserRecipes = ({ username }) => (
+
+    <Query query={GET_USER_RECIPES} variables={{ username }}>
+    {({ data, loading, error }) => {
+      if (loading) return <Spinner />;
+      if (error) return <div>Error</div>;
+      return (
+        <ul>
+          <h3>Your Recipes</h3>
+          {!data.getUserRecipes.length && (
+            <p>
+              <strong>You have not added any recipes yet</strong>
+            </p>
+          )}
+          {data.getUserRecipes.map(recipe => (
+            <li key={recipe._id}>
+              <Link to={`/recipes/${recipe._id}`}>
+                <p>{recipe.name}</p>
+              </Link>
+              <p style={{ marginBottom: "0" }}>Likes: {recipe.likes}</p>
+            </li>
+          ))}
+        </ul>
+      );
+    }}
+ 
+ </Query>
+)
 
 
-// const UserRecipes = ({ username }) => (
-
-
-// )
-
-
-// export default UserRecipes;
+export default UserRecipes;
--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,12 +1,13 @@
     
 import React from "react";
 import UserInfo from "./UserInfo"
+import UserRecipes from "./UserRecipe"
 const Profile = ({ session }) => (
     <div className="App">
     profile
 
       <UserInfo session={session} />
-      {/* <UserRecipes username={session.getCurrentUser.username} /> */}
+      <UserRecipes username={session.getCurrentUser.username} />
     </div>
   );
 
