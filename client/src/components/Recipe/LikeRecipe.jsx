fae00affd35cc0e8f10c4cfee8a8b8ac6ca77bbc
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-05-03 23:16:03 +0600

    like client

--- client/src/components/Recipe/LikeRecipe.jsx
+++ client/src/components/Recipe/LikeRecipe.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 
 import { Mutation } from "react-apollo";
 import withSession from "../withSession";
+import { LIKE_RECIPE} from "../../queries";
 class LikeRecipe extends Component {
 
     state = {
@@ -9,21 +10,45 @@ class LikeRecipe extends Component {
         username: ""
       };
 
+      componentDidMount() {
+        if (this.props.session.getCurrentUser) {
+          const { username, favorites } = this.props.session.getCurrentUser;
+          this.setState({
+              username,
+              favorites
+          })
+        }
+      }
       
+      handleClick = (likeRecipe) => {
+      likeRecipe().then(({ data}) => {
+console.log(data)
+      })
+      };
     
     render () {
-console.log(this.props)
         const { liked, username } = this.state;
         const { _id } = this.props;
-        return(
-            <button>
-                Like
-            </button>
 
-        )
+        return (
+            <Mutation
+              mutation={LIKE_RECIPE}
+              variables={{ _id, username }}
+            //   update={this.updateUnlike}
+            >
+                  {likeRecipe =>
+                    username && (
+                      <button
+                        className="like-button"
+                        onClick={() => this.handleClick(likeRecipe)}
+                      >
+                       Like
+                      </button>
+                    )
+                  }
+            </Mutation>
+          );
     }
-
-
 }
 
 export default withSession(LikeRecipe)
