ff86fd69ba0324cd35b881a74dab840f011ae622
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-05-03 20:28:45 +0600

    schema for likes

--- schema.js
+++ schema.js
@@ -35,6 +35,7 @@ type Query {
 }
 
 type Mutation {
+    likeRecipe(_id: ID!, username: String!): Recipe
     addRecipe(name:String!, imageUrl:String!, description:String! category: String!, instructions: String!, username: String): Recipe,
     signupUser(username:String!, email:String!, password:String!):Token
     signinUser(username: String!, password: String!): Token
